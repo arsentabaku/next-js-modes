4f8c273ad452ea094a487414d02b03805ab17963
Arsen Tabaku <arsen.tabaku@vrdirect.com>
2022-08-18 10:51:54 +0200

    Setup home for CSR

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    async function getPokemon() {
+      const resp = await fetch(
+        "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
+      );
+      setPokemon(await resp.json());
+    }
+    getPokemon();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pokemon List</title>
+      </Head>
+      <h2>Pokemon List</h2>
+      <div className={styles.grid}>
+        {pokemon.map((pokemon) => (
+          <div className={styles.card} key={pokemon.id}>
+            <Link href={`/pokemon/${pokemon.id}`}>
+              <a>
+                <img
+                  src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+                  alt={pokemon.name}
+                />
+                <h3>{pokemon.name}</h3>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
